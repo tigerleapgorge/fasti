791e60eed3cc5e607e8e91ab0890baf70b8b87de
tigerleapgorge <tigerleapgorge@gmail.com>
2016-05-16 23:16:07 -0700

    Got interpret for functions

--- fasti.js
+++ fasti.js
@@ -65,6 +65,61 @@
         return retArray;
     }
     
+    var library = {
+        "+" : function(x, y) {
+                console.log("plus", x, y, x[0], x[1]);
+                return x[0] + x[1];
+              }
+        
+    }
+    
+    var Context = function(scope, parent) {
+        this.scope = scope;
+        this.parent = parent;
+        
+        this.get = function(identifier){
+          if (identifier in this.scope) {
+              return this.scope[identifier];
+          } else if (this.parent !== undefined) {
+              return this.parently.get(indentifier); // recursive to the top.
+          }
+        };
+    }
+    
+    var interpretList = function(input, context) {
+        
+        // non-special form
+        console.log("before:", input);
+        var list = input.map( // interpret every node in the list
+            function(x) { 
+                return interpret(x, context); 
+            }               );
+        console.log("after: ", list);
+        if (list[0] instanceof Function) { // invoke the function
+            console.log("list:", list);
+            var newlist = list.slice(1);
+            console.log("newlist:", newlist);
+            // each array element maps to a variable in the function, like intrinsics 
+            var myres = list[0].apply(undefined, list.slice(1)); 
+            console.log(myres);
+            return myres;
+        }
+    }
+    
+    var interpret = function(input, context) {
+        if(context === undefined) {
+            return interpret(input, new Context(library) )
+        }
+        if(input instanceof Array) {
+            return interpretList(input); // recursive decent
+        } else if (input.type === "identifier") {
+            return context.get(input.value);
+        } else if (input.type === "number") {
+            return input.value;
+        }
+        
+    }
+    
     // Main Loop
     function main(){
         tokenArray = sourceCode.replace(/\(/g, " ( ")
@@ -77,6 +132,10 @@
 
         ast = parenthesize(tokenArray);
         console.log(ast);
+
+        var res = interpret(ast);
+        console.log(res);
+
         drawAll();
     }
     //setInterval(drawAll, 16);  // run faster for debugging

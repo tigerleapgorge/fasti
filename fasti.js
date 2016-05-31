9a8a19a2711a2e2038196a594b3bcc09070b924a
tigerleapgorge <tigerleapgorge@gmail.com>
2016-05-31 00:02:59 -0700

    Shitty spring simulation
    Things just fly apart.

--- fasti.js
+++ fasti.js
@@ -5,9 +5,9 @@
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
 /*******                Data                     ******/
-    var sourceCode = "( ( lambda ( a b ) (+ a b) ) 8 55 )";
+    //var sourceCode = "( ( lambda ( a b ) (+ a b) ) 8 55 )";
     //var sourceCode = "( ( lambda (x) x) 3)";
-    //var sourceCode = "(+ 3 5)";
+    var sourceCode = "(+ 3 5)";
     var tokenArray = [];
     var ast = [];
 
@@ -24,6 +24,27 @@
 	vector.prototype.add = function(vecArg) {
         return new vector(this.x + vecArg.x, this.y + vecArg.y);
 	};
+    
+	vector.prototype.subtract = function(v2) {
+		return new vector(this.x - v2.x, this.y - v2.y);
+	};
+
+	vector.prototype.multiply = function(n) {
+		return new vector(this.x * n, this.y * n);
+	};
+    
+	vector.prototype.divide = function(n) {
+		return new vector( (this.x / n) || 0,
+                           (this.y / n) || 0);
+	};
+    
+	vector.prototype.magnitude = function() {
+		return Math.sqrt(this.x*this.x + this.y*this.y);
+	};
+    
+	vector.prototype.normalize = function() {
+		return this.divide(this.magnitude());
+	};
 
     var deltaRightVector = new vector(50,0);
     var deltaDownVector = new vector(0,50);
@@ -36,7 +57,7 @@
     }
 
     function drawRect(position){
-        console.log("drawRect ", position);
+        //console.log("drawRect ", position);
         ctx.fillStyle = "green";
         ctx.fillRect(position.x, position.y, 20, 20);
 
@@ -82,7 +103,7 @@
                 }
                 return sum;
               }
-    }
+    };
     
     var Context = function(scope, parent) {
         this.scope = scope;
@@ -95,7 +116,7 @@
               return this.parent.get(identifier); // recursive to the top.
           }
         };
-    }
+    };
 
 /*******                Interpreter                      ******/
     var special = {
@@ -128,7 +149,7 @@
                 return lambda_res;
             }
         }
-    }
+    };
 
     var interpretList = function(input, context) {
         console.log("INTERPRET LIST: ", input);
@@ -152,7 +173,7 @@
                 return list;
             }
         }
-    }
+    };
     
     var interpret = function(input, context) {
         console.log("INTERPRET: ", input);
@@ -172,32 +193,32 @@
             console.log("is number: ", input);
             return input.value;
         }
-    }
+    };
 
     var visualizeList = function(input, position) {
         var curPosition = position;
         for(var i = 0; i < input.length; i++){
-            console.log("visualizeList ", curPosition);
+            //console.log("visualizeList ", curPosition);
             visualize(input[i], curPosition);
             curPosition = curPosition.add(deltaRightVector);
         }
         return;
-    }
+    };
 
     var visualize = function(input, position) {
-        console.log("In visualize");
+        //console.log("In visualize");
         if (position === undefined) {
-            console.log("Vis no position");
+            //console.log("Vis no position");
             return visualize(input, new vector(canvas.width/2,  canvas.height/8) );
         } else if (input === undefined) {
-            console.log("Vis no input");
+            //console.log("Vis no input");
             return;
         } else if (input instanceof Array) {
-            console.log("Vis Array");
+            //console.log("Vis Array");
             visualizeList(input, position);
             return;
         } else { 
-            console.log("Vis Else", input.pos, position);
+            //console.log("Vis Else", input.pos, position);
             if (input.pos === undefined) {
                 input.pos = new vector(position.x, position.y);
             }
@@ -212,38 +233,149 @@
             }
             return;
         }
-    }
-    
-    var applySpring = function(posA, posB) {
+    };
+
+   
+    var applySpring = function(inputA, inputB) {
+        var d = inputA.pos.subtract(inputB.pos);
+        var displacement = 20 - d.magnitude(); // spring length is 50
+        var direction = d.normalize();
+        
+        console.log("inputA.pos ", inputA.pos);
+        console.log("inputB.pos ", inputB.pos);
+        console.log("d ", d);
+        console.log("d.magnitude ", d.magnitude);
+        console.log("d.normalize ", d.normalize);
+        console.log("direction ", direction);
+        console.log("displacement", displacement);
+        
+        if (inputA.a === undefined){
+            inputA.a = new vector(0, 0);
+        }
+        if (inputB.a === undefined){
+            inputB.a = new vector(0, 0);
+        }
+        
+        console.log("inputA.a ", inputA.a);
+        
+        inputA.a = inputA.a.add( direction.multiply(0.05 * displacement * -0.5 ) );
+        inputB.a = inputB.a.add( direction.multiply(0.05 * displacement * 0.5 ) );
+        
+        console.log("inputA.a ", inputA.a);
+        console.log(" direction.multiply(50 * displacement * -0.5 )",  direction.multiply(50 * displacement * -0.5 ) );
+        console.log("inputB.a ", inputB.a);
+        console.log("inputA.a ", inputA.a);
+        
         return;
-        
-        
-    }
+    };
+
+    var timestep = 0.01; // Time Step -- super important
+
+    var updateVelocity = function(input) {
+        console.log("updateVelocity");
+        console.log("updateVelocity input", input);
+        if(input instanceof Array) {
+            updateVelocityList(input);
+        } else {
+            if(input.a === undefined){
+                console.log("updateVelocity lack acceleration", input);
+            } else {
+                if(input.v === undefined) { // if veloctiy don't exist, initialize it
+                    input.v = new vector(0,0);
+                }
+                input.v = input.v.add( input.a.multiply(timestep) ); // Core
+                
+                 
+                console.log("Update Velocity: ", input);
+                console.log("Update Velocity acceleration: ", input.a);
+                console.log("Update Velocity velocity: ", input.v);
+                if(input.type === "expr") { // is paren atom
+                    updateVelocityList(input.sexpr); // DFS
+                }
+            }
+        }
+    };
+    var updateVelocityList = function(input) {
+        console.log("updateVelocityList");
+        for(var i = 0; i < input.length; i++){
+            updateVelocity(input[i]);
+        }
+    };
+
+    var updatePosition = function(input) {
+        console.log("updatePosition", input);
+        if(input instanceof Array) {
+            updatePositionList(input);
+        } else {
+            if (input.v !== undefined) { // make sure velocity exist
+                /*
+                if(input.pos === undefined) { // if veloctiy don't exist, initialize it
+                    input.pos = new vector(0,0);
+                }
+                */
+                input.pos = input.pos.add( input.v.multiply(timestep) ); // Core 
+                if(input.type === "expr") { // is paren atom
+                    updatePositionList(input.sexpr); // DFS
+                    
+                }
+            }
+        }
+    };
+    var updatePositionList = function(input) {
+        console.log("updatePositionList", input);
+        for(var i = 0; i < input.length; i++){
+            updatePosition(input[i]);
+        }
+    };
+    
     
     var springList = function(input, prev) {
-        console.log("springList");
+        //console.log("springList");
         if (input === undefined ) {
-            console.log("springList NO input");
+            //console.log("springList NO input");
             return;
         } else if ( !(input instanceof Array) ) {
-            console.log("springList Not Array");
+            //console.log("springList Not Array");
             return;
         } else {
-            console.log("input.length: ", input.length);
+            //console.log("input.length: ", input.length);
+            if(prev !== undefined){
+                for(var i = 0; i < input.length; i++) {
+                    applySpring(input[i], prev);
+                }
+            }
+            
             for(var i = 1; i < input.length; i++) {
-                console.log("springList action: ", input[i-1]);
-                applySpring(input[i-1].pos, input[i].pos);
-                input[i-1].pos.x += 1;
+                //console.log("springList action: ", input[i-1]);
+                // input[i-1].pos.x += 1; shift to the right by 1px
+                applySpring(input[i], input[i-1]);
             }
             for(var i = 0; i < input.length; i++) {
-                console.log("springList Recurse: ", input[i]);
+                //console.log("springList Recurse: ", input[i]);
                 if (input[i].type === "expr") {
                     springList(input[i].sexpr, input[i]);
                 }
             }
         }
         
-    }
+    };
+    
+    var DFT = function(input) {
+        if (input instanceof Array) {
+            DFT_list(input);
+        } else {
+            console.log("DFT ", input);
+            if(input.type === "expr") {
+                DFT_list(input.sexpr);
+            }
+        }
+    };
+    
+    var DFT_list = function(input) {
+        for(var i = 0; i <= input.length; i++){
+            DFT(input[i]);
+        }
+    };
     
 /*******                Main Loop                      ******/
     function main(){
@@ -260,20 +392,38 @@
         console.log("ast ", ast);
         console.log("type of ast: ", typeof ast);
         console.log("ast is array: ", ast instanceof Array);
-                
-        var drawCall = function() {
-            console.log("drawCall");
+
+/*
             ctx.clearRect(0, 0, canvas.width, canvas.height); // clear screen
             visualize(ast);
             springList(ast);
-        }
-        window.setInterval(drawCall, 500);
-        
-        visualize(ast);
-        springList(ast);
+            
+            console.log("----------------------");
+            updateVelocityList(ast);
+            updatePositionList(ast);
+            console.log("++++++++++++++++++++++");
+*/
 
-        var final_res = interpret(ast);
-        console.log("Final Result: ", final_res);
+  
+
+        var frame = 0;
+        var drawCall = function() {
+            
+            console.log("drawCall", frame++);
+            ctx.clearRect(0, 0, canvas.width, canvas.height); // clear screen
+            visualize(ast);
+            springList(ast);
+            updateVelocityList(ast);
+            updatePositionList(ast);
+        }
+        window.setInterval(drawCall, 50);
+
+
+//        visualize(ast);
+//        springList(ast);
+
+//        var final_res = interpret(ast);
+//        console.log("Final Result: ", final_res);
 
         //drawAll();
     }
